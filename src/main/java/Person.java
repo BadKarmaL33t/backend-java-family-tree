378ed65feb4455e808f6001e65bcbccd66901df4
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-06-26 16:46:39 +0200

    huiswerkopdracht uitwerken

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,134 @@
-public class Person {
+import java.util.ArrayList;
+import java.util.List;
+
+class Person {
+
+    private final String name;
+    private String middleName;
+    private final String lastName;
+    private String sex;
+    private int age;
+    private Person mother;
+    private Person father;
+    private List<Person> siblings;
+    private List<Person> children;
+    private List<Pet> pets;
+
+    public Person(String name, String lastName, int age, String sex) {
+        this.name = name;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public Person(String name, String middleName, String lastName, int age, String sex) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public List<Person> getSiblings() {
+        return siblings;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public void setSiblings(List<Person> siblings) {
+        this.siblings = siblings;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public void addParents(Person father, Person mother, Person child){
+        child.setMother(mother);
+        mother.addChild(mother, child);
+        child.setFather(father);
+        father.addChild(father, child);
+    }
+
+    public void addChild(Person parent, Person child){
+        List<Person> children = new ArrayList<>();
+        if(parent.getChildren()!= null) {
+            children.addAll(parent.getChildren());
+        }
+        children.add(child);
+        parent.setChildren(children);
+    }
+
+    public void addPet(Person person, Pet pet){
+        List<Pet> pets = new ArrayList<>();
+        if(person.getPets() != null){
+            pets.addAll(person.getPets());
+        }
+        pets.add(pet);
+        person.setPets(pets);
+    }
+
+    public void addSibling(Person person, Person sibling){
+        List<Person> siblings = new ArrayList<>();
+        if(person.getSiblings() != null){
+            siblings.addAll(person.getSiblings());
+        }
+        siblings.add(sibling);
+        person.setSiblings(siblings);
+    }
 }
+
